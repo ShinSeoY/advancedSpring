c8d3aa3d851544d29e3396973f848da3a36db34a
sandy <=>
2023-11-12 13:36:44 +0900

    init

---
+++ src/test/java/com/sandy/advancedSpring/AdvancedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandy.advancedSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
