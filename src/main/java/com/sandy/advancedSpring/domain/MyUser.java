2d9fd6ea91ee09da4d04b2526c998ca153bed146
sandy <=>
2024-01-09 10:40:48 +0900

    - custom exception 설정

---
+++ src/main/java/com/sandy/advancedSpring/domain/MyUser.java
@@ -0,0 +1,35 @@
+package com.sandy.advancedSpring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "MY_USER")
+public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String username;
+
+    String password;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/sandy/advancedSpring/exception/ErrorCodeHandler.java
@@ -0,0 +1,76 @@
+package com.sandy.advancedSpring.exception;
+
+import com.sandy.advancedSpring.exception.error.InvalidValueException;
+import com.sandy.advancedSpring.exception.error.NotFoundException;
+import com.sandy.advancedSpring.exception.error.UserException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.file.AccessDeniedException;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorCodeHandler {
+
+
+    // 개별 exception 설정할 때 사용
+    @ExceptionHandler(InvalidValueException.class)
+    private ResponseEntity<Object> handleInvalidValueException(final InvalidValueException e, final HttpServletRequest httpServletRequest) {
+        ErrorResponse response = ErrorResponse.of(e.getErrorCode(), httpServletRequest.getRequestURI());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+        return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    private ResponseEntity<Object> handleNotFoundException(final NotFoundException e, final HttpServletRequest httpServletRequest) {
+        ErrorResponse response = ErrorResponse.of(e.getErrorCode(), httpServletRequest.getRequestURI());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+        return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+    }
+
+    // 한번에 error 설정할 때 사용
+    @ExceptionHandler(UserException.class)
+    private ResponseEntity<ErrorCode> handleUserException(final UserException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("handleUserException... {}", errorCode);
+        return new ResponseEntity<>(errorCode, errorCode.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    private ResponseEntity<Object> handleAccessDeniedException(final AccessDeniedException e, final HttpServletRequest httpServletRequest) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.UNAUTHORIZED, httpServletRequest.getRequestURI());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+        return new ResponseEntity<>(response, ErrorCode.UNAUTHORIZED.getHttpStatus());
+    }
+
+    @ExceptionHandler(BindException.class)
+    private ResponseEntity<Object> handleBindException(final BindException e, final HttpServletRequest httpServletRequest) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID, httpServletRequest.getRequestURI(), e.getBindingResult());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+        return new ResponseEntity<>(response, ErrorCode.INVALID.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<Object> handleException(final Exception e, final HttpServletRequest httpServletRequest) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.UNCAUGHT, httpServletRequest.getRequestURI());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+        return new ResponseEntity<>(response, ErrorCode.UNCAUGHT.getHttpStatus());
+    }
+
+}
