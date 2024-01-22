9a199ecd597a61f6b6b559d9964e45bedf91fe89
ShinSeoY <tjdudtls312@gmail.com>
2024-01-22 13:30:07 +0900

    - 기존 멀티데이터베이스 트랜잭션 설정 주석처리 - JTATransactionManager 로 멀티 데이터베이스 트랜잭션 설정

--- src/main/java/com/sandy/advancedSpring/common/config/database/ChainedTransactionConfig.java
+++ src/main/java/com/sandy/advancedSpring/common/config/database/ChainedTransactionConfig.java
@@ -1,22 +1,22 @@
-package com.sandy.advancedSpring.common.config.database;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.transaction.ChainedTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@Configuration
-public class ChainedTransactionConfig {
-
-    @Autowired
-    @Bean
-    public PlatformTransactionManager chainedTransactionManager(
-            @Qualifier("myAdminTransactionManager") PlatformTransactionManager myAdminTransactionManager,
-            @Qualifier("myUserTransactionManager") PlatformTransactionManager myUserTransactionManager
-    ) {
-        return new ChainedTransactionManager(myAdminTransactionManager, myUserTransactionManager);
-    }
-
-}
+//package com.sandy.advancedSpring.common.config.database;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.transaction.ChainedTransactionManager;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//@Configuration
+//public class ChainedTransactionConfig {
+//
+//    @Autowired
+//    @Bean
+//    public PlatformTransactionManager chainedTransactionManager(
+//            @Qualifier("myAdminTransactionManager") PlatformTransactionManager myAdminTransactionManager,
+//            @Qualifier("myUserTransactionManager") PlatformTransactionManager myUserTransactionManager
+//    ) {
+//        return new ChainedTransactionManager(myAdminTransactionManager, myUserTransactionManager);
+//    }
+//
+//}
